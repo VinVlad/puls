6709c6bc7c488f45973443d054f378e31f192c81
Vinokurov.V.V <vinokurovvlad.26@gmail.com>
2022-04-13 21:54:30 +1000

    Половина слайдера. Добавил в js левую стрелку слайдера

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+	speed: 1200,
+	adaptiveHeight: true,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" ></button>'
+  });
+});
