e92b6c074ef5eb8d3888360b08f786abfd79a30e
Vinokurov.V.V <vinokurovvlad.26@gmail.com>
2022-04-14 22:05:23 +1000

    Закончил слайдер

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,17 @@ $(document).ready(function(){
   $('.carousel__inner').slick({
 	speed: 1200,
 	adaptiveHeight: true,
-	autoplay: true,
-	autoplaySpeed: 2000,
-	prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg" ></button>'
+	// autoplay: true,
+	// autoplaySpeed: 2000,
+	prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
+	nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+			dots: true,
+			arrows: false
+		}
+	}]
   });
 });
